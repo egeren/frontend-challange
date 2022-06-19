6db24410588a8d3d542a33edba55cf5d9f9216df
Ege Eren <egeren@egeren.local>
2022-06-19 12:13:43 +0300

    project initialized. webpack, eslint, tailwind and prettier configured

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+function Landing() {
+  return (
+    <div className="landing-wrapper relative w-full min-h-screen">
+      <img
+        src="/images/background.png"
+        className="absolute w-full h-full object-cover object-left -z-10"
+      />
+      <div className="landing-container w-full">
+        <div className="texts-container">
+          <h1 className="w-fit uppercase font-bebas text-8xl tracking-widest">
+            SEARCH FOR EVENTS
+          </h1>
+          <p className="font-poppins font-normal text-white text-2xl">
+            You can search concerts, theaters, festivals, whatever you want.
+          </p>
+          <p className="font-poppins font-normal text-white text-2xl">
+            Select your aviability date, event type and see details of it.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      bebas: ['Bebas Neue', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};

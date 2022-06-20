21f2e270cb6f4bf5251c2aaa73526c054834ade7
Ege Eren <egeren@egeren.local>
2022-06-20 22:55:22 +0300

    react-redux & redux toolkit init, configured

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import Landing from './pages/Landing';
 import ComponentPage from './pages/ComponentPage';
 import Events from 'pages/Events';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
 
 function App() {
-  return <Events />;
+  return (
+    <Provider store={store}>
+      <Landing />
+    </Provider>
+  );
 }
 
 export default App;

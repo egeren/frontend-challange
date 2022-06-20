0a987389c03a2f2411654860505dd8ab28b7a155
Ege Eren <egeren@egeren.local>
2022-06-20 23:07:41 +0300

    react router init, configured

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import ComponentPage from './pages/ComponentPage';
 import Events from 'pages/Events';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/components" element={<ComponentPage />} />
+          <Route path="/events" element={<Events />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
